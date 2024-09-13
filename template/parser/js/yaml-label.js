1a6879d6e3407762fd13eec70c9cdef56f1c80b6
modelearth <model@dreamstudio.com>
2024-09-12 21:31:21 -0400

    Wait for div for yaml loading

--- template/parser/js/yaml-label.js
+++ template/parser/js/yaml-label.js
@@ -1,17 +1,4 @@
-const profileDiv = document.createElement('div');
-profileDiv.classList.add('envProfile');
 
-// Add profile type
-const profileTypeDiv = document.createElement('div');
-profileTypeDiv.classList.add('profileType');
-profileTypeDiv.textContent = 'Product Profile';
-profileDiv.appendChild(profileTypeDiv);
-
-// Add profile title
-const profileTitleDiv = document.createElement('div');
-profileTitleDiv.classList.add('profileTitle');
-profileTitleDiv.textContent = 'Product Title'; // Hardcoded for now
-profileDiv.appendChild(profileTitleDiv);
 
 // Helper function to format URLs
 const formatUrl = (url) => {
@@ -60,69 +47,86 @@ function isEmpty(obj) {
 
 // Loop through yamlData and create divs for each key-value pair
 function formatYamlLabel(data,profileContainer) {
-  const yamlData = jsyaml.load(data);
-  Object.entries(yamlData).forEach(([key, value]) => {
-    if (value !== null && typeof value !== 'object') {
-      const itemDiv = document.createElement('div');
-      const span = document.createElement('span');
-      if (key.includes('url') || key == 'ref') {
-        span.innerHTML = formatUrl(value);
-      } else if (key.includes('date')) {
-        span.textContent = formatDate(value);
-      } else {
-        span.textContent = value;
-      }
-      itemDiv.innerHTML = `<b>${formatKey(key)}</b> `;
-      itemDiv.appendChild(span);
-      profileDiv.appendChild(itemDiv);
-    } else if (typeof value === 'object' && value !== null && !isEmpty(value)) {
-      const itemDiv = document.createElement('div');
-      const keyDiv = document.createElement('div');
-      keyDiv.innerHTML = `<b>${formatKey(key)}</b> `;
-      itemDiv.appendChild(keyDiv);
+  waitForElm('#formatted').then((elm) => {
 
-      // Loop through nested object
-      Object.entries(value).forEach(([subKey, subValue]) => {
-        if (subValue !== null && typeof subValue !== 'object') {
-          const subItemDiv = document.createElement('div');
-          const subSpan = document.createElement('span');
-          if (subKey.includes('url') || subKey == 'ref') {
-            subSpan.innerHTML = formatUrl(subValue);
-          } else if (subKey.includes('date')) {
-            subSpan.textContent = formatDate(subValue);
-          } else {
-            subSpan.textContent = subValue;
-          }
-          subItemDiv.innerHTML = `${formatKey(subKey)} `;
-          subItemDiv.appendChild(subSpan);
-          itemDiv.appendChild(subItemDiv);
-        } else if (typeof subValue === 'object' && subValue !== null)  {
-          const subItemDiv = document.createElement('div');
-          const subSpan = document.createElement('span');
-          subSpan.innerHTML = `<b>${formatKey(key)}</b> `;
-          subItemDiv.appendChild(keyDiv);
-          Object.entries(subValue).forEach(([subSubKey, subSubValue]) => {
-            if (subSubValue !== null) {
+    const profileDiv = document.createElement('div');
+    profileDiv.classList.add('envProfile');
 
-              const subSubItemDiv = document.createElement('div');
-              const subSubSpan = document.createElement('span');
-              if (subSubKey.includes('url') || subSubKey == 'ref') {
-                subSubSpan.innerHTML = formatUrl(subSubValue);
-              } else if (subSubKey.includes('date')) {
-                subSubSpan.textContent = formatDate(subSubValue);
-              } else {
-                subSubSpan.textContent = subSubValue;
-              }
-              subSubItemDiv.innerHTML = `${formatKey(subSubKey)} `;
-              subSubItemDiv.appendChild(subSubSpan);
-              subItemDiv.appendChild(subSubItemDiv);
-            }
-          });
+    // Add profile type
+    const profileTypeDiv = document.createElement('div');
+    profileTypeDiv.classList.add('profileType');
+    profileTypeDiv.textContent = 'Product Profile';
+    profileDiv.appendChild(profileTypeDiv);
+
+    // Add profile title
+    const profileTitleDiv = document.createElement('div');
+    profileTitleDiv.classList.add('profileTitle');
+    profileTitleDiv.textContent = 'Product Title'; // Hardcoded for now
+    profileDiv.appendChild(profileTitleDiv);
+
+    const yamlData = jsyaml.load(data);
+    Object.entries(yamlData).forEach(([key, value]) => {
+      if (value !== null && typeof value !== 'object') {
+        const itemDiv = document.createElement('div');
+        const span = document.createElement('span');
+        if (key.includes('url') || key == 'ref') {
+          span.innerHTML = formatUrl(value);
+        } else if (key.includes('date')) {
+          span.textContent = formatDate(value);
+        } else {
+          span.textContent = value;
         }
-      });
+        itemDiv.innerHTML = `<b>${formatKey(key)}</b> `;
+        itemDiv.appendChild(span);
+        profileDiv.appendChild(itemDiv);
+      } else if (typeof value === 'object' && value !== null && !isEmpty(value)) {
+        const itemDiv = document.createElement('div');
+        const keyDiv = document.createElement('div');
+        keyDiv.innerHTML = `<b>${formatKey(key)}</b> `;
+        itemDiv.appendChild(keyDiv);
 
-      profileDiv.appendChild(itemDiv);
-    }
+        // Loop through nested object
+        Object.entries(value).forEach(([subKey, subValue]) => {
+          if (subValue !== null && typeof subValue !== 'object') {
+            const subItemDiv = document.createElement('div');
+            const subSpan = document.createElement('span');
+            if (subKey.includes('url') || subKey == 'ref') {
+              subSpan.innerHTML = formatUrl(subValue);
+            } else if (subKey.includes('date')) {
+              subSpan.textContent = formatDate(subValue);
+            } else {
+              subSpan.textContent = subValue;
+            }
+            subItemDiv.innerHTML = `${formatKey(subKey)} `;
+            subItemDiv.appendChild(subSpan);
+            itemDiv.appendChild(subItemDiv);
+          } else if (typeof subValue === 'object' && subValue !== null)  {
+            const subItemDiv = document.createElement('div');
+            const subSpan = document.createElement('span');
+            subSpan.innerHTML = `<b>${formatKey(key)}</b> `;
+            subItemDiv.appendChild(keyDiv);
+            Object.entries(subValue).forEach(([subSubKey, subSubValue]) => {
+              if (subSubValue !== null) {
+
+                const subSubItemDiv = document.createElement('div');
+                const subSubSpan = document.createElement('span');
+                if (subSubKey.includes('url') || subSubKey == 'ref') {
+                  subSubSpan.innerHTML = formatUrl(subSubValue);
+                } else if (subSubKey.includes('date')) {
+                  subSubSpan.textContent = formatDate(subSubValue);
+                } else {
+                  subSubSpan.textContent = subSubValue;
+                }
+                subSubItemDiv.innerHTML = `${formatKey(subSubKey)} `;
+                subSubItemDiv.appendChild(subSubSpan);
+                subItemDiv.appendChild(subSubItemDiv);
+              }
+            });
+          }
+        });
+        profileDiv.appendChild(itemDiv);
+      }
+    });
+    profileContainer.appendChild(profileDiv);
   });
-  profileContainer.appendChild(profileDiv);
 }
