f4b271bd7cc62b3e86fc6b316b94d08ef2e77a53
Dhananjay Surti <djsurti3003@gmail.com>
2024-10-30 11:51:35 -0400

    Nav script command line

--- scripts/add-nav/add_nav_wrapper.py
+++ scripts/add-nav/add_nav_wrapper.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+import argparse
 
 # Define the path to your HTML file
 input_html_path = '/Users/dhananjaysurti/model_earth/useeio-widgets/src/html/impact_chart_config.html'
@@ -18,17 +18,25 @@ function hashChangedUseeio() {
     // pageScript();
 }
 </script>
+
+<link type="text/css" rel="stylesheet" href="/localsite/css/base.css" id="/localsite/css/base.css" />
+<script type="text/javascript" src="/localsite/js/localsite.js?showheader=true"></script>
 """
 
+# Set up argument parsing
+parser = argparse.ArgumentParser(description="Add a script tag to the <head> of an HTML file.")
+parser.add_argument("html_path", help="Path to the HTML file to modify.")
+args = parser.parse_args()
+
 # Read the original HTML file
-with open(input_html_path, 'r', encoding='utf-8') as file:
+with open(args.html_path, 'r', encoding='utf-8') as file:
     html_content = file.read()
 
 # Insert the script tag content before the closing </head> tag
 updated_html_content = html_content.replace('</head>', script_tag_content + '\n</head>')
 
 # Save the modified HTML to a new file
-with open(output_html_path, 'w', encoding='utf-8') as file:
+with open(args.html_path, 'w', encoding='utf-8') as file:
     file.write(updated_html_content)
 
 print("Script tag added successfully without altering original indentation!")
